f5d6f263849e5dc1cfc70c3324561be4a78f35ce
Daniel Shi <danielshi@Daniels-MBP.lan>
2024-05-08 05:41:29 -0700

    Adding new menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,45 @@ import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import "./App.scss";
 import { useGSAP } from "@gsap/react";
-import Landing from "./components/landing";
+import Landing from "./components/loading";
 
 function App() {
-  function splitTextIntoSpans(selector: any) {
-    let elements = document.querySelectorAll(selector);
-    elements.forEach((element) => {
-      let text = element.innerText;
-      let splitText = text
-        .split("")
-        .map(function (char: string) {
-          return `<span>${char === " " ? "&nbsp;&nbsp;" : char}</span>`;
-        })
-        .join("");
-      element.innerHTML = splitText;
-    });
-  }
+  // function splitTextIntoSpans(selector: any) {
+  //   let elements = document.querySelectorAll(selector);
+  //   elements.forEach((element) => {
+  //     let text = element.innerText;
+  //     let splitText = text
+  //       .split("")
+  //       .map(function (char: string) {
+  //         return `<span>${char === " " ? "&nbsp;&nbsp;" : char}</span>`;
+  //       })
+  //       .join("");
+  //     element.innerHTML = splitText;
+  //   });
+  // }
 
-  useGSAP(() => {
-    // gsap.to(".slider", {
-    //   delay: 5,
-    //   display: "none", 
-    // });
-    gsap.to(".slide-next-img", {
-      delay: 5,
-      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-      duration: 1.5,
-      ease: "power3.out",
-    });
-    splitTextIntoSpans(".slider-content-active h1");
-    gsap.to(".slider-content-active h1 span", {
-      delay: 5.5,
-      duration: 1,
-      stagger: 0.05,
-      top: "0",
-      ease: "power3.out",
-    });
-    gsap.to(".slide-active img", {
-      delay: 5.5,
-      scale: 2,
-      duration: 2,
-      ease: "power3.out",
-    });
-  });
+  // useGSAP(() => {
+  //   gsap.to(".slide-next-img", {
+  //     delay: 5,
+  //     clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+  //     duration: 1.5,
+  //     ease: "power3.out",
+  //   });
+  //   splitTextIntoSpans(".slider-content-active h1");
+  //   gsap.to(".slider-content-active h1 span", {
+  //     delay: 5.5,
+  //     duration: 1,
+  //     stagger: 0.05,
+  //     top: "0",
+  //     ease: "power3.out",
+  //   });
+  //   gsap.to(".slide-active img", {
+  //     delay: 5.5,
+  //     scale: 2,
+  //     duration: 2,
+  //     ease: "power3.out",
+  //   });
+  // });
 
   return (
     <div>
@@ -65,6 +61,31 @@ function App() {
           <a href="#">Cart</a>
         </div>
       </nav>
+      <div className="container">
+        <nav></nav>
+
+        <div className="items">
+          <div className="item">
+            <p>Item Name</p>
+          </div>
+          <div className="item">
+            <p>Item Name</p>
+          </div>
+          <div className="item">
+            <p>Item Name</p>
+          </div>
+          <div className="item">
+            <p>Item Name</p>
+          </div>
+          <div className="item">
+            <p>Item Name</p>
+          </div>
+        </div>
+
+        <div className="preview-bg">
+          <img src="/images/1.jpg" alt="" />
+        </div>
+      </div>
       {/* <div className="copy">
         <p>Lifestyle Catalogue</p>
         <p>2024 / 2025</p>
@@ -72,7 +93,7 @@ function App() {
       {/* <footer>
         <p>Launching Soon</p>
       </footer> */}
-      <div className="slider">
+      {/* <div className="slider">
         <div className="slide-active">
           <img src="/images/3.jpg" />
         </div>
@@ -86,7 +107,7 @@ function App() {
             <h1 className="">Title</h1>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
